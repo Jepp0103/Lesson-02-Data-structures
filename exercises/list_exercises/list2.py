6b066090d7285124ac38cfe56260eb4b43037c2a
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-02-17 18:03:04 +0100

    ...

--- exercises/list_exercises/list2.py
+++ exercises/list_exercises/list2.py
@@ -12,9 +12,11 @@
 
 def remove_adjacent(nums):
     unique_nums = []
+
     for num in nums:
         if num not in unique_nums:
             unique_nums.append(num)
+            
     return unique_nums
 
 
