ac49a896c39596c7b4b27656a0f3bc0bc3739a3b
Jepp0103 <new email>
2020-02-17 17:41:37 +0100

    ...

--- exercises/list_exercises/list1.py
+++ exercises/list_exercises/list1.py
@@ -17,8 +17,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return 
+  count = 0
+  for word in words:
+      if len(word) >= 2 and word[0] == word[len(word) - 1]:
+        count += 1
+  return count
 
 
 # B. front_x
@@ -29,23 +32,25 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return 
+  no_x = []
+  x = []
 
-
+  for word in words:
+    if(word[0] == "x"):
+      x.append(word)
+    else:
+      no_x.append(word)
+      
+  return sorted(x) + sorted(no_x)
 
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
 # order by the last element in each tuple.
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
-# Hint: use a custom key= function to extract the last element form each tuple.
-
-
-
+# Hint: use a custom key= function to extract the last element from each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return 
+    return sorted(tuples, key=lambda x: x[1])
 
 
 
--- exercises/list_exercises/list2.py
+++ exercises/list_exercises/list2.py
@@ -10,10 +10,12 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 
-
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+    unique_nums = []
+    for num in nums:
+        if num not in unique_nums:
+            unique_nums.append(num)
+    return unique_nums
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -21,8 +23,7 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-    # +++your code here+++
-    return
+    return sorted(list1 + list2)
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
