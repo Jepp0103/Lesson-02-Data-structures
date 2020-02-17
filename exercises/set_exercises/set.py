79eaf801b42db06f9fa3431cb2a36a59c3ae72ee
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-02-17 20:57:08 +0100

    ...

--- exercises/set_exercises/set.py
+++ exercises/set_exercises/set.py
@@ -6,64 +6,118 @@
 
 
 # 1. Write a Python program to create a set.
-print("-------------------------------------------------")
-print("Exercise 1")
-programmingLanguages = set(['Python', 'Java', 'Scala', 'C#', 'SQL', 'JavaScript', "HTML"])
-print(programmingLanguages)
+programmingLanguages = set(['Python', 'Java', 'C#'])
+
+#'Scala', 'SQL', 'JavaScript', "HTML
 
 # 2. Write a Python program to iteration over sets.
-print("-------------------------------------------------")
-print("Exercise 2")
 
+def iterateSet():
+    for programmingLanguage in programmingLanguages:
+        print(programmingLanguage)
 
 # 3. Write a Python program to add member(s) in a set. 
-print("-------------------------------------------------")
-print("Exercise 3")
+def addLanguage(programmingLanguage):
+    programmingLanguages.add(programmingLanguage)
+    return programmingLanguages
+
 # 4. Write a Python program to remove item(s) from set. 
-print("-------------------------------------------------")
-print("Exercise 4")
+def removeLanguage(programmingLanguage):
+#remove method to remove specific element in set
+    programmingLanguages.remove(programmingLanguage)
+    return programmingLanguages
+
+
 # 5. Write a Python program to remove an item from a set if it is present in the set. 
-print("-------------------------------------------------")
-print("Exercise 5")
+def removeLanguageIfExists(programmingLanguage):
+    if programmingLanguage in programmingLanguages:
+        programmingLanguages.remove(programmingLanguage)
+        return programmingLanguages
+    else:
+        return "Language not found"
+
+    
+
+
 # 6. Write a Python program to create an intersection of sets.
-print("-------------------------------------------------")
-print("Exercise 6")
+
 # ![](https://www.linuxtopia.org/online_books/programming_books/python_programming/images/p2c6-intersection.png)
 
 # 7. Write a Python program to create a union of sets.
-print("-------------------------------------------------")
-print("Exercise 7")
+
 #  ![](https://www.linuxtopia.org/online_books/programming_books/python_programming/images/p2c6-union.png)
 
 # 8. Write a Python program to create set difference.
-print("-------------------------------------------------")
-print("Exercise 8")
+
 #  ![](https://www.linuxtopia.org/online_books/programming_books/python_programming/images/p2c6-difference.png)
 
 # 9. Write a Python program to create a symmetric difference.
-print("-------------------------------------------------")
-print("Exercise 9")
+
 #  ![](https://www.linuxtopia.org/online_books/programming_books/python_programming/images/p2c6-symmdiff.png)
 
 # 10. Write a Python program to issubset and issuperset.
-print("-------------------------------------------------")
-print("Exercise 10")
+
 
 
 # 11. Write a Python program to create a shallow copy of sets.
-print("-------------------------------------------------")
-print("Exercise 11")
+
 # Note : Shallow copy is a bit-wise copy of an object. A new object is created that has an exact copy of the values in the original object.
 
 # 12. Write a Python program to clear a set.
-print("-------------------------------------------------")
-print("Exercise 12")
+
 # 13. Write a Python program to use of frozensets.
-print("-------------------------------------------------")
-print("Exercise 13")
+
 # 14. Write a Python program to find maximum and the minimum value in a set.
-print("-------------------------------------------------")
-print("Exercise 14")
+
 # 15. Write a Python program to find the length of a set.
-print("-------------------------------------------------")
-print("Exercise 15")
+
+
+def main():
+    print("-------------------------------------------------")
+    print("Exercise 1")
+    print(programmingLanguages)
+
+    print("-------------------------------------------------")
+    print("Exercise 2")
+    print(iterateSet())
+
+    print("-------------------------------------------------")
+    print("Exercise 3")
+
+    print("-------------------------------------------------")
+    print("Exercise 4")
+
+    print("-------------------------------------------------")
+    print("Exercise 5")
+
+    print("-------------------------------------------------")
+    print("Exercise 6")
+
+    print("-------------------------------------------------")
+    print("Exercise 7")
+
+    print("-------------------------------------------------")
+    print("Exercise 8")
+
+    print("-------------------------------------------------")
+    print("Exercise 9")
+
+    print("-------------------------------------------------")
+    print("Exercise 10")
+
+    print("-------------------------------------------------")
+    print("Exercise 11")
+
+    print("-------------------------------------------------")
+    print("Exercise 12")
+
+    print("-------------------------------------------------")
+    print("Exercise 13")
+
+    print("-------------------------------------------------")
+    print("Exercise 14")
+
+    print("-------------------------------------------------")
+    print("Exercise 15")
+main()
+
